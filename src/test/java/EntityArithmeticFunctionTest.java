0bce670b8b1ee1a57a7576fc26ce8373b48ca190
Burdishka <oleksandr.burdeinyi@gmail.com>
2020-12-13 20:40:40 -0500

    fixed delete method and gave priority=2, added 4 tests, made assertions in separate methods, added methods for buttons clicking, added methods for setting values

--- src/test/java/EntityArithmeticFunctionTest.java
+++ src/test/java/EntityArithmeticFunctionTest.java
@@ -15,27 +15,105 @@ public class EntityArithmeticFunctionTest extends BaseTest {
 
         ProjectUtils.login(driver, "user1@tester.com", "ah1QNmgkEO");
 
+        clickArithmeticFunction(driver);
+    }
+
+    public void clickArithmeticFunction(WebDriver driver) {
+
         WebElement tabArithmeticFunction =
                 driver.findElement(By.xpath("//div[@id = 'menu-list-parent']//p[text() = ' Arithmetic Function ']"));
         tabArithmeticFunction.click();
     }
 
-    public void recordDelete(WebDriver driver) throws InterruptedException {
+    public void createNewRecord(WebDriver driver) {
 
-        WebElement tabArithmeticFunction =
-                driver.findElement(By.xpath("//div[@id = 'menu-list-parent']//p[text() = ' Arithmetic Function ']"));
-        tabArithmeticFunction.click();
+        WebElement createNewRecord = driver.findElement(By.xpath("//div[@class='card-icon']"));
+        createNewRecord.click();
+    }
 
-        Thread.sleep(3000);
+    public void setValuesF1F2(WebDriver driver, int f1, int f2) {
+
+        WebElement inputF1 = driver.findElement(By.id("f1"));
+        inputF1.clear();
+        inputF1.sendKeys(String.valueOf(f1));
+
+        WebElement inputF2 = driver.findElement(By.id("f2"));
+        inputF2.clear();
+        inputF2.sendKeys(String.valueOf(f2));
+    }
+
+    public void setValuesF1F2String(WebDriver driver, String f1, String f2) {
+
+        WebElement inputF1 = driver.findElement(By.id("f1"));
+        inputF1.clear();
+        inputF1.sendKeys(String.valueOf(f1));
+
+        WebElement inputF2 = driver.findElement(By.id("f2"));
+        inputF2.clear();
+        inputF2.sendKeys(String.valueOf(f2));
+    }
+
+    public void clickMenuOfLastRecord(WebDriver driver) {
 
         List <WebElement> allRecords = driver.findElements(By.xpath("//tr[@data-index]"));
         int length = allRecords.size();
 
         WebElement lastRecord = driver.findElement(By.xpath("//tr[@data-index][" + length +"]//td[8]"));
         lastRecord.click();
+    }
+
+    public void clickViewMode(WebDriver driver) {
+
+        WebElement viewMode = driver.findElement(By.xpath("//ul[@x-placement='bottom-end']//a[text()='view']"));
+        ProjectUtils.click(driver, viewMode);
+    }
+
+    public void clickEditMode(WebDriver driver) {
+
+        WebElement editMode = driver.findElement(By.xpath("//ul[@x-placement='bottom-end']//a[text()='edit']"));
+        ProjectUtils.click(driver, editMode);
+    }
+
+    public void clickDelete(WebDriver driver) {
 
         WebElement deleteRecord = driver.findElement(By.xpath("//ul[@x-placement='bottom-end']//a[text()='delete']"));
-        deleteRecord.click();
+        ProjectUtils.click(driver, deleteRecord);
+    }
+
+    public void clickSaveButton(WebDriver driver) {
+
+        WebElement saveButton = driver.findElement(By.id("pa-entity-form-save-btn"));
+        saveButton.click();
+    }
+
+    public void valuesAssertViewMode(WebDriver driver, int f1, int f2) {
+
+        WebElement sum = driver.findElement(By.xpath("//div[3]//span"));
+        Assert.assertEquals(sum.getText(), String.valueOf(f1 + f2));
+
+        WebElement sub = driver.findElement(By.xpath("//div[4]//span"));
+        Assert.assertEquals(sub.getText(), String.valueOf(f1 - f2));
+
+        WebElement mul = driver.findElement(By.xpath("//div[5]//span"));
+        Assert.assertEquals(mul.getText(), String.valueOf(f1 * f2));
+
+        WebElement div = driver.findElement(By.xpath("//div[6]//span"));
+        Assert.assertEquals(div.getText(), String.valueOf(f1 / f2));
+    }
+
+    public void valuesAssertEditMode(WebDriver driver, int f1, int f2) {
+
+        WebElement sum = driver.findElement(By.id("sum"));
+        Assert.assertEquals(sum.getAttribute("value"), String.valueOf(f1 + f2));
+
+        WebElement sub = driver.findElement(By.id("sub"));
+        Assert.assertEquals(sub.getAttribute("value"), String.valueOf(f1 - f2));
+
+        WebElement mul = driver.findElement(By.id("mul"));
+        Assert.assertEquals(mul.getAttribute("value"), String.valueOf(f1 * f2));
+
+        WebElement div = driver.findElement(By.id("div"));
+        Assert.assertEquals(div.getAttribute("value"), String.valueOf(f1 / f2));
     }
 
     @Test
@@ -43,68 +121,107 @@ public class EntityArithmeticFunctionTest extends BaseTest {
 
         WebDriver driver = getDriver();
         preconditions(driver);
-
-        WebElement createNewRecord = driver.findElement(By.xpath("//div[@class='card-icon']"));
-        createNewRecord.click();
+        createNewRecord(driver);
 
         final int f1 = 12;
         final int f2 = 6;
 
-        WebElement inputF1 = driver.findElement(By.id("f1"));
-        inputF1.sendKeys(String.valueOf(f1));
+        setValuesF1F2(driver, f1, f2);
 
-        WebElement inputF2 = driver.findElement(By.id("f2"));
-        inputF2.sendKeys(String.valueOf(f2));
         Thread.sleep(1000);
 
-        WebElement sum = driver.findElement(By.id("sum"));
-        Assert.assertTrue(String.valueOf(f1 + f2).equals(sum.getAttribute("value")));
+        valuesAssertEditMode(driver, f1, f2);
 
-        WebElement sub = driver.findElement(By.id("sub"));
-        Assert.assertTrue(String.valueOf(f1 - f2).equals(sub.getAttribute("value")));
-
-        WebElement mul = driver.findElement(By.id("mul"));
-        Assert.assertTrue(String.valueOf(f1 * f2).equals(mul.getAttribute("value")));
-
-        WebElement div = driver.findElement(By.id("div"));
-        Assert.assertTrue(String.valueOf(f1 / f2).equals(div.getAttribute("value")));
-
-        WebElement buttonSave = driver.findElement(By.id("pa-entity-form-save-btn"));
-        buttonSave.click();
+        clickSaveButton(driver);
     }
 
     @Test
-    public void tc002 () throws InterruptedException {
+    public void tc002 () {
 
         WebDriver driver = getDriver();
         preconditions(driver);
-
-        List <WebElement> allRecords = driver.findElements(By.xpath("//tr[@data-index]"));
-        int length = allRecords.size();
-
-        WebElement lastRecord = driver.findElement(By.xpath("//tr[@data-index][" + length +"]//td[8]"));
-        lastRecord.click();
-
-        WebElement viewMode = driver.findElement(By.xpath("//ul[@x-placement='bottom-end']//a[text()='view']"));
-        viewMode.click();
+        clickMenuOfLastRecord(driver);
+        clickViewMode(driver);
 
         final int f1 = 12;
         final int f2 = 6;
 
-        WebElement sum = driver.findElement(By.xpath("//div[3]//span"));
-        Assert.assertTrue(String.valueOf(f1 + f2).equals(sum.getText()));
+        valuesAssertViewMode(driver, f1, f2);
+    }
 
-        WebElement sub = driver.findElement(By.xpath("//div[4]//span"));
-        Assert.assertTrue(String.valueOf(f1 - f2).equals(sub.getText()));
+    @Test
+    public void tc003() throws InterruptedException {
 
-        WebElement mul = driver.findElement(By.xpath("//div[5]//span"));
-        Assert.assertTrue(String.valueOf(f1 * f2).equals(mul.getText()));
+        WebDriver driver = getDriver();
+        preconditions(driver);
+        clickMenuOfLastRecord(driver);
+        clickEditMode(driver);
 
-        WebElement div = driver.findElement(By.xpath("//div[6]//span"));
-        Assert.assertTrue(String.valueOf(f1 / f2).equals(div.getText()));
+        final int f1 = 24;
+        final int f2 = 6;
 
-        //recordDelete(driver);
+        setValuesF1F2(driver, f1, f2);
 
-        Thread.sleep(3000);
+        Thread.sleep(1000);
+
+        valuesAssertEditMode(driver, f1, f2);
+
+        clickSaveButton(driver);
+    }
+
+    @Test
+    public void tc004() {
+
+        WebDriver driver = getDriver();
+        preconditions(driver);
+        clickMenuOfLastRecord(driver);
+        clickViewMode(driver);
+
+        final int f1 = 24;
+        final int f2 = 6;
+
+        valuesAssertViewMode(driver, f1, f2);
+    }
+
+    @Test
+    public void tc005() {
+
+        WebDriver driver = getDriver();
+        preconditions(driver);
+        clickMenuOfLastRecord(driver);
+        clickEditMode(driver);
+
+        final int f1 = 24;
+        final int f2 = 6;
+
+        valuesAssertEditMode(driver, f1, f2);
+    }
+
+    @Test
+    public void tc006() {
+
+        WebDriver driver = getDriver();
+        preconditions(driver);
+        clickMenuOfLastRecord(driver);
+        clickEditMode(driver);
+
+        final String f1 = "f1";
+        final String f2 = "f2";
+
+        setValuesF1F2String(driver, f1, f2);
+
+        clickSaveButton(driver);
+
+        WebElement error = driver.findElement(By.id("pa-error"));
+        Assert.assertEquals(error.getText(), "Error saving entity");
+    }
+
+    @Test(priority=2)
+    public void recordDelete(){
+
+        WebDriver driver = getDriver();
+        preconditions(driver);
+        clickMenuOfLastRecord(driver);
+        clickDelete(driver);
     }
 }
